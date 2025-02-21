516a6492a21761d3da3375968a9704d03e3b7144
ManuelPeva <manuel_859@outlook.es>
2025-02-21 00:52:33 -0600

    prueba

--- src/components/TicketForm.jsx
+++ src/components/TicketForm.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import SignatureCanvas from "react-signature-canvas";
 import logo from "../assets/logo.png";
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/blume-tech",
   plugins: [react()],
   build: {
     rollupOptions: {
